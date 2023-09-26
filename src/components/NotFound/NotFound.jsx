02941571190cff8669c044484cf933ba7c137766
Ian0001 <ianvrdoljak@hotmail.es>
2023-09-26 16:15:04 -0300

    Agregué 2 componentes: Detail y NotFound

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+
+    // Para implementar agregar esto como una Route en App.jsx:
+    // <Route path="*" element={<NotFound/>} />
+    // Cualquier ruta no especificada renderizará NotFound
+
+    const navigate = useNavigate();
+    const goHome = () => {
+        navigate("/")//puse "/" como la ruta de home, cambiarlo si no es así
+    };
+
+    return(
+        <div className="notfound_container">
+            <h1>Oops! Página no encontrada</h1>
+            <h2>404</h2>
+            <h2>No podemos encontrar la página que buscaste</h2>
+            <button onClick={goHome}>Volver a Home</button>
+        </div>
+    )
+};
+export default NotFound;
