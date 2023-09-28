cf8714cc50e22972459f73867be810a0dbb4a57e
Ian0001 <ianvrdoljak@hotmail.es>
2023-09-28 17:44:16 -0300

    Agregué un a route a app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Router, Route} from "react-router-dom"
 import Login from './Component/Login/Login'
 import Paginado from './Component/Paginado/Paginado'
 import NonRegisteredHome from './Views/NonRegisteredHome/NonRegisteredHome'
+import NotFound from "./Component/NotFound/NotFound"
 function App() {
 
 
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route exact path='/login' element={<Login/>}/>
         <Route exact path='/paginado' element={<Paginado/>}/>
-        <Route exact path='/' element={<NonRegisteredHome/>}/> 
+        <Route exact path='/' element={<NonRegisteredHome/>}/>
+        <Route path="*" element={<NotFound/>}/> 
       </Routes>
     </div>
   )
