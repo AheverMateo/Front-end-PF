9a2376ee9122f8f6483c5f92ac2c01ba7b0485ed
salvador <salvador.cahuana3@gmail.com>
2023-10-03 15:02:43 -0300

    fix-sidebar

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,12 @@
 import ListCards from "../ListCards/ListCards";
 import SearchBar from "../SearchBar/SearchBar";
-
+import SideBar from "../SideBar/SideBar";
 import style from "./Home.module.css";
 
 const Home = () => {
     return(
         <div className={style.home}>
+            <SideBar />
             <ListCards />
             <SearchBar/>
         </div>
