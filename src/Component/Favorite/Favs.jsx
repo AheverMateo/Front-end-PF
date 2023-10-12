a2cb30b8af3665d214757b50f67498ddb8cd108a
Franco V <frava46254425mail.com>
2023-10-12 15:13:56 -0300

    Boton rapido al carrito

--- src/Component/Favorite/Favs.jsx
+++ src/Component/Favorite/Favs.jsx
@@ -1,12 +1,20 @@
 
+import { useEffect } from "react";
 import Filters from "../Filters/Filters";
 import ListCards from "../ListCards/ListCards";
 import SearchBar from "../SearchBar/SearchBar";
 import SideBar from "../SideBar/SideBar";
 import Favorites from "./Favorites";
 import style from "./Favorites.module.css";
+import { useDispatch } from "react-redux";
+import { cleanFavs } from "../../Redux/actions/actions";
 
 const Favs = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    return ()=> {dispatch(cleanFavs())}
+  }, []);
   return (
     <div className={style.home}>
       <SideBar />
